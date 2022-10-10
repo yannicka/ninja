b801e3d9cdbec2d514f9d8c1cbd1cc217c930b68
Yannick Armand <yannick.armand@protonmail.com>
2022-10-10 23:00:53 +0200

    Préparation des tortues

---
+++ src/Enemies/Turtle.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+const SPEED = 20
+
+var direction = Vector2.RIGHT
+
+func _ready():
+	velocity.x = SPEED
+
+func _physics_process(delta):
+	move_and_slide()
+
+	if not is_on_floor():
+		velocity.y += 120 * delta
+
+	if is_on_wall():
+		direction = -direction
+		velocity.x = SPEED * direction.x
+
+		scale.x = -1
+
+func _on_area_2d_body_entered(body):
+	if body.has_method("die_or_bounce"):
+		body.die_or_bounce()
+		queue_free()
