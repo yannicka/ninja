5fd9dd8f56b038156e89f064851285ab7372811e
Yannick Armand <yannick.armand@protonmail.com>
2022-10-12 22:06:51 +0200

    Ajout d'une page de crédits

---
+++ src/Scenes/Credits.gd
@@ -0,0 +1,4 @@
+extends Control
+
+func _on_back_button_pressed():
+	TransitionScene.change_scene("res://src/Scenes/MainMenu.tscn")
--- src/Scenes/MainMenu.gd
+++ src/Scenes/MainMenu.gd
@@ -2,3 +2,6 @@ extends Control
 
 func _on_play_button_pressed():
 	TransitionScene.change_scene("res://src/Scenes/Game.tscn")
+
+func _on_credits_button_pressed():
+	TransitionScene.change_scene("res://src/Scenes/Credits.tscn")
