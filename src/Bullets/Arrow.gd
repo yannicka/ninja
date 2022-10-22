539fdd087499b8d433e604c8039ee270b459255b
Yannick Armand <yannick.armand@protonmail.com>
2022-10-22 15:00:27 +0200

    La flèche se colle au mur

--- src/Bullets/Arrow.gd
+++ src/Bullets/Arrow.gd
@@ -28,11 +28,6 @@ func _physics_process(delta):
 	move_and_slide()
 
 	if velocity.x == 0:
-		velocity = Vector2.ZERO
-		$CollisionShape2d.set_deferred("disabled", true)
-		# $top_checker/CollisionShape2d.set_deferred("disabled", true)
-		# $sides_checker/CollisionShape2d.set_deferred("disabled", true)
-
 		$AnimatedSprite2d.play("stop")
 
 func _on_top_checker_body_entered(body):
