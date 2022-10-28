5007dd04f1d4f6eef37a78dcd4104950bf2abb50
Yannick Armand <yannick.armand@protonmail.com>
2022-10-28 20:51:13 +0200

    Préparation du code qui permet de modifier la couleur du dégradé en fond

--- src/Scenes/Game.gd
+++ src/Scenes/Game.gd
@@ -1,5 +1,16 @@
 extends Node2D
 
+# var i = 0
+#
+# func _physics_process(delta: float) -> void:
+# 	i += delta / 20
+#
+# 	if i > 1:
+# 		i = 0
+#
+# 	$CanvasLayer2/Polygon2D.get_texture().gradient.set_color(0, Color(i, i, 0.5, 1))
+# 	$CanvasLayer2/Polygon2D.get_texture().gradient.set_color(1, Color(i, 0.5, i, 1))
+
 func _input(event: InputEvent) -> void:
 	if event is InputEventScreenTouch:
 		var viewport_size = get_viewport_rect().size
