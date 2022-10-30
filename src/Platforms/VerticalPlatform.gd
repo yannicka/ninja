029eca1c30ac392ca882d92f7c680506218f7fe0
Yannick Armand <yannick.armand@protonmail.com>
2022-10-30 22:43:07 +0100

    Plateforme verticale supprimée quand elle sort de l'écran

--- src/Platforms/VerticalPlatform.gd
+++ src/Platforms/VerticalPlatform.gd
@@ -5,6 +5,11 @@ const VELOCITY = Vector2(0, 20)
 func _physics_process(delta: float) -> void:
 	move_and_collide(VELOCITY * delta)
 
+	var viewport_height = get_viewport_rect().size.y
+
+	if global_position.y > viewport_height + 20:
+		queue_free()
+
 # Bouge la plateforme à la position à laquelle elle serait si elle s'était
 # déplacée pendant `seconds` secondes.
 func move(seconds: float) -> void:
